394bf9be598324b7c29fc8b3d1d8fab4e984373b
NicoTimothy11 <nico.timothy319@gmail.com>
2023-08-21 21:00:38 +0800

    Let's see if this works

--- character.cpp
+++ character.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class GameSprite {
+public:
+	void showName(string name) {
+		cout << name << endl;
+	}
+};
+
+int main() {
+	GameSprite obj;
+	obj.showName("Nico");
+	return 0;
+}
