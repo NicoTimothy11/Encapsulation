b7f5b8ad6957723a5762323b67fc4d81c139d458
NicoTimothy11 <nico.timothy319@gmail.com>
2023-08-22 15:12:46 +0800

    new changes

--- character.cpp
+++ character.cpp
@@ -4,14 +4,45 @@
 using namespace std;
 
 class GameSprite {
+private:
+	int health;
+	int armor;
+	string weapon;
+	string type;
 public:
-	void showName(string name) {
-		cout << name << endl;
+	GameSprite(int health, int armor, string weapon) {
+		this->health = health;
+		this->armor = armor;
+		this->weapon = weapon;
+	}
+
+	void setHealth(int newHealth) {
+		health = newHealth;
+	}
+
+	void setArmor(int newArmor) {
+		armor = newArmor;
+	}
+
+	void setWeapon(string newWeapon) {
+		weapon = newWeapon;
+	}
+
+	int getHealth() {
+		return health;
+	}
+
+	int getArmor() {
+		return armor;
+	}
+	string getWeapon() {
+		return weapon;
 	}
 };
 
 int main() {
-	GameSprite obj;
-	obj.showName("Nico");
+	GameSprite Dino(3, 6, "jaws");
+	cout << "Health: " << Dino.getHealth();
 	return 0;
 }
+
